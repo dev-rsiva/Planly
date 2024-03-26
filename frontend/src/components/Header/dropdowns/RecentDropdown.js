3d168b990b894c47753d4385ecce649a2045f624
dev-rsiva <dev.rsiva@gmail.com>
2024-03-26 19:25:42 +0530

    hide recently viewed boards when its empty

--- frontend/src/components/Header/dropdowns/RecentDropdown.js
+++ frontend/src/components/Header/dropdowns/RecentDropdown.js
@@ -7,61 +7,70 @@ const Recent = ({ setNavItemStatus }) => {
   const { workspaceData, setWorkspaceData } = useContext(dataContext);
 
   const navigate = useNavigate();
+
+  const recentlyViewedBoards = workspaceData.workspaces
+    .map((workspace) => {
+      return workspace?.boards?.filter((eachBoard, index) => {
+        return eachBoard?.viewedAt !== "" && !eachBoard.starred;
+      });
+    })
+    .flat();
+
   return (
     <div className="absolute top-[160%] left-[0%] rounded-md shadow-2xl min-w-[308px] border border-gray-200 py-2 px-2 bg-white z-[1000]">
-      {workspaceData.workspaces
-        .map((workspace) => {
-          return workspace?.boards?.filter((eachBoard, index) => {
-            return eachBoard?.viewedAt !== "" && !eachBoard.starred;
-          });
-        })
-        .flat()
-        .sort((a, b) => b.viewedAt - a.viewedAt)
-        .slice(0, 4)
-        .map((eachBoard, index) => {
-          return (
-            <div
-              className="flex items-center py-2 px-2 rounded hover:bg-gray-100"
-              onClick={() => {
-                setNavItemStatus((prev) => {
-                  let updatedNavItemStatus = [...prev];
+      {recentlyViewedBoards.length === 0 ? (
+        <p className="font-sans text-sm text-custom font-semibold">
+          You have no recently viewed boards
+        </p>
+      ) : (
+        recentlyViewedBoards
+          .sort((a, b) => b.viewedAt - a.viewedAt)
+          .slice(0, 4)
+          .map((eachBoard, index) => {
+            return (
+              <div
+                className="flex items-center py-2 px-2 rounded hover:bg-gray-100"
+                onClick={() => {
+                  setNavItemStatus((prev) => {
+                    let updatedNavItemStatus = [...prev];
 
-                  updatedNavItemStatus[1] = {
-                    ...updatedNavItemStatus[1],
-                    dropdownIsShowing: false,
-                  };
+                    updatedNavItemStatus[1] = {
+                      ...updatedNavItemStatus[1],
+                      dropdownIsShowing: false,
+                    };
 
-                  return updatedNavItemStatus;
-                });
-                navigate(
-                  `/b/${eachBoard.id}/${eachBoard.title.replace(/ /g, "-")}`
-                );
-              }}
-            >
-              <img
-                src={eachBoard.backgroundImg}
-                className="w-[40px] h-[33px] rounded mr-2"
-              />
-              <div className="flex flex-col flex-1 mr-[2px]">
-                <p
-                  className="text-sm font-sans text-[#172b4d] font-semibold overflow-hidden whitespace-nowrap overflow-ellipsis cursor-pointer"
-                  style={{ maxWidth: "210px" }}
-                >
-                  {eachBoard?.title}
-                </p>
-                <p className="text-xs font-sans text-custom">
-                  {
-                    workspaceData?.workspaces?.find((workspace) =>
-                      workspace?.boards?.some(
-                        (board) => board.id === eachBoard.id
-                      )
-                    ).name
-                  }
-                </p>
+                    return updatedNavItemStatus;
+                  });
+                  navigate(
+                    `/b/${eachBoard.id}/${eachBoard.title.replace(/ /g, "-")}`
+                  );
+                }}
+              >
+                <img
+                  src={eachBoard.backgroundImg}
+                  className="w-[40px] h-[33px] rounded mr-2"
+                />
+                <div className="flex flex-col flex-1 mr-[2px]">
+                  <p
+                    className="text-sm font-sans text-[#172b4d] font-semibold overflow-hidden whitespace-nowrap overflow-ellipsis cursor-pointer"
+                    style={{ maxWidth: "210px" }}
+                  >
+                    {eachBoard?.title}
+                  </p>
+                  <p className="text-xs font-sans text-custom">
+                    {
+                      workspaceData?.workspaces?.find((workspace) =>
+                        workspace?.boards?.some(
+                          (board) => board.id === eachBoard.id
+                        )
+                      ).name
+                    }
+                  </p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+      )}
     </div>
   );
 };
--- frontend/src/components/Main/RecentlyViewedBoards.js
+++ frontend/src/components/Main/RecentlyViewedBoards.js
@@ -15,39 +15,45 @@ const RecentlyViewedBoards = ({
 
   console.log(workspaceData);
 
+  const recentlyViewedBoards = workspaceData?.workspaces
+    .map((workspace) => {
+      return workspace?.boards?.filter((eachBoard, index) => {
+        return eachBoard?.viewedAt !== "" && !eachBoard?.starred;
+      });
+    })
+    .flat();
+
   return (
-    <div className="mb-12">
-      <div className="flex justify-start items-center mb-4">
-        <FontAwesomeIcon
-          icon={faClock}
-          className="text-gray-600 mr-3 text-lg"
-        />
-        <h1 className="font-bold font-sans text-base text-[#172b4d]">
-          Recently viewed
-        </h1>
-      </div>
-      <div className="flex flex-wrap">
-        {workspaceData?.workspaces
-          .map((workspace) => {
-            return workspace?.boards?.filter((eachBoard, index) => {
-              return eachBoard?.viewedAt !== "" && !eachBoard?.starred;
-            });
-          })
-          .flat()
-          .sort((a, b) => b.viewedAt - a.viewedAt)
-          .slice(0, 4)
-          .map((eachBoard, index) => {
-            console.log("fffff");
-            return (
-              <CommonBoard
-                key={index}
-                board={eachBoard}
-                typeOfBoard="recentlyViewed"
-              />
-            );
-          })}
-      </div>
-    </div>
+    <>
+      {recentlyViewedBoards.length > 0 && (
+        <div className="mb-12">
+          <div className="flex justify-start items-center mb-4">
+            <FontAwesomeIcon
+              icon={faClock}
+              className="text-gray-600 mr-3 text-lg"
+            />
+            <h1 className="font-bold font-sans text-base text-[#172b4d]">
+              Recently viewed
+            </h1>
+          </div>
+          <div className="flex flex-wrap">
+            {recentlyViewedBoards
+              .sort((a, b) => b.viewedAt - a.viewedAt)
+              .slice(0, 4)
+              .map((eachBoard, index) => {
+                console.log("fffff");
+                return (
+                  <CommonBoard
+                    key={index}
+                    board={eachBoard}
+                    typeOfBoard="recentlyViewed"
+                  />
+                );
+              })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
